adf4e7558fedc5a10a50c364c81b76d3b275fc06
Zachary Zhuo <zachary@webcomm.com.tw>
2022-02-25 17:58:59 +0800

    init project

---
+++ src/main/java/com/zachary/ticketgrabbingtool/TicketGrabbingToolApplication.java
@@ -0,0 +1,13 @@
+package com.zachary.ticketgrabbingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketGrabbingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketGrabbingToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zachary/ticketgrabbingtool/TicketGrabbingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachary.ticketgrabbingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGrabbingToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
