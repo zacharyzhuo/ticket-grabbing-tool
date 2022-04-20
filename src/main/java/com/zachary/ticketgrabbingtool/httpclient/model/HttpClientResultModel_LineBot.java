b68cb25ebb488120f583e453d397d3ebf047f442
ZacharyZhuo <zachary@webcomm.com.tw>
2022-04-20 12:46:07 +0800

    [rent591] 補上HttpClientResultModel_LineBot

---
+++ src/main/java/com/zachary/ticketgrabbingtool/httpclient/model/HttpClientResultModel_LineBot.java
@@ -0,0 +1,9 @@
+package com.zachary.ticketgrabbingtool.httpclient.model;
+
+public class HttpClientResultModel_LineBot extends HttpClientResultModel {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}
